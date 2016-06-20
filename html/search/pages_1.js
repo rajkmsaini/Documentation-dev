0765cf3f936c26a9cc62b3441bf2f6066e4e31dd
Henry Weller <http://cfd.direct>
2016-06-20 19:28:14 +0100

    Updated

---
+++ html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['free_2c_20open_20source_20software_20from_20the_20openfoam_20foundation',['Free, Open Source Software from the OpenFOAM Foundation',['../index.html',1,'']]]
+];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -12,7 +12,7 @@ var indexSectionsWithContent =
   9: "abcdefghilmnoprstu",
   10: "_abcdefghiklmnoprstuvw",
   11: "bcfgilorstuw",
-  12: "b"
+  12: "bf"
 };
 
 var indexSectionNames =
